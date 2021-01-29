fc1d49ba9598e59b22ddbea21fe052586b7ff928
Steve Burton <steveburtonwebdev@gmail.com>
2021-01-29 12:47:41 +0000

    initial files added

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function changeImage(imgClicked) {
+        var imgChange = "img" + imgClicked;
+        document.getElementById(imgChange).style.opacity = 0.4; 
+           
+}
+
+function resetImages() {
+    document.getElementById("img1").style.opacity = 1; 
+    document.getElementById("img2").style.opacity = 1; 
+    document.getElementById("img3").style.opacity = 1; 
+    document.getElementById("img4").style.opacity = 1; 
+    document.getElementById("img5").style.opacity = 1; 
+    document.getElementById("img6").style.opacity = 1; 
+
+}
